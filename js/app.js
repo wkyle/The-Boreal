8d9fd4581d643a7f5e478e250f6ed2f36011319a
wkyle <wrkyle@mail.com>
2017-06-11 11:17:01 -0600

    .

--- js/app.js
+++ js/app.js
@@ -231,7 +231,7 @@ function initMapBoxMap(mapdata) {
     var corner2 = L.latLng(bbox[3], bbox[2])
     var mapbounds = L.latLngBounds(corner1, corner2).pad(0.005)
 
-    var mymap = L.map('mapboxmap', {attributionControl: false, zoomDelta: 0.5});
+    var mymap = L.map('mapboxmap', {attributionControl: false, zoomDelta: 0.2, zoomSnap: 0.2});
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: '',
@@ -240,7 +240,7 @@ function initMapBoxMap(mapdata) {
     }).addTo(mymap);
     L.geoJson(mapdata).addTo(mymap);
     mymap.fitBounds(mapbounds)
-    mymap.options.zoomDelta = .2;
+    // mymap.options.zoomDelta = .2;
     console.log(corner1)
     console.log(corner2)
 }
