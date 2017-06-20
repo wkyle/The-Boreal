ce209365c405f12c9e91f758e0db74c64ed7fc00
wkyle <wrkyle@mail.com>
2017-06-19 19:22:05 -0600

    .

--- js/app.js
+++ js/app.js
@@ -5,42 +5,46 @@ $(document).foundation()
 
 
 
-//*****************************************************************
+//*******************************************************************************************
 //   PAGE GLOBAL VARIABLES
-//*****************************************************************
+//*******************************************************************************************
 
-
+var siteroot = "The-Boreal/";
 var electionsXML;
 var geocoder;
 
 
 
-//*****************************************************************
+//*******************************************************************************************
 //   PAGE INITIALIZATION
-//*****************************************************************
+//*******************************************************************************************
 
 
 function initializeMainPage() {
     createPageWaypoints();
     loadXML();
     initLocationSearchBar();
+    initElectionCountdown()
 }
 
 function initializeProvincePage() {
     createPageWaypoints();
     initLocationSearchBar();
-    var province = provinceFromQueryString();
-    var mapdataURI = "../data/shapefiles/" + province + "/" + province + "-multiPart-simplified.json";
+    var province = queryStringObject().province;
+    var mapdataURI = siteroot + "/data/shapefiles/" + province + "/" + province + "-multiPart-simplified.json";
     d3.json(mapdataURI, function (er, mapdata) {
         initMapBoxMap(mapdata);
     });
+    initElectionCountdown()
     loadXML();
 }
 
 function initializeFEDPage() {
+    var fedID = queryStringObject().fed;
     createPageWaypoints();
-    loadXML();
+    initElectionCountdown()
     initLocationSearchBar();
+    loadXML();
 }
 
 
@@ -62,25 +66,6 @@ function initLocationSearchBar() {
     autocomplete = new google.maps.places.Autocomplete(input, options);    
 }
 
-function provinceFromQueryString() {
-    var query = window.location.search;
-    if (query.substring(0, 1) == '?') {
-        query = query.substring(1);
-    }
-    var data = decodeURI(query);
-    var province = data.slice(-2);    
-    return province;
-}
-
-function FEDFromQueryString() {
-    var query = window.location.search;
-    if (query.substring(0, 1) == '?') {
-        query = query.substring(1);
-    }
-    var data = decodeURI(query);
-    var FED = data.slice(-5);    
-    return FED;
-}
 
 
 function createPageWaypoints() {
@@ -149,36 +134,27 @@ function createPageWaypoints() {
 //*****************************************************************
 
 
-$( ".postal-search-bar button" ).click(function() {
+$(".postal-search-bar button").click(function() {
 
   var string = $(".postal-search-bar input").val();
   if (Boolean(string)) {
     var address = $("#address-input").val();
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == 'OK') {
-            var fedID = coord2FED([results[0].geometry.location.lng(), results[0].geometry.location.lat()]);
-
-            if (Boolean(fedID)) {
-                var queryString = "?fedid=" + String(fedID);
-                var url = encodeURI("./FEDs" + queryString);
-                window.location = url;
-            } else {
-                window.location = "#province-map";
-                callBadAddressPopup();
-            }
-
+            coord2FED([results[0].geometry.location.lng(), results[0].geometry.location.lat()]);
         }
     });
 
   }
 });
 
-
-$(document).ready(function(){
-    $(".FED-titlebar-flex").click(function(e){
-        $(this).siblings(".FED-body-flex").toggle();
-        return;
-    });
+$(document).on("click", ".FED-titlebar-flex", function() {
+    var fedID = $(this).children(".FED-name").attr("id");
+    if (Boolean(fedID)) {
+        var queryString = "?fedid=" + String(fedID);
+        var url = encodeURI(siteroot + "/FEDs" + queryString);
+        window.location = url;
+    }
 });
 
 
@@ -186,18 +162,103 @@ $(document).ready(function(){
 
 
 
+//*******************************************************************************************
+//   CONVENIENCE FUNCTIONS
+//*******************************************************************************************
+
+function getRandomColor(color, variance) {
+    var blue = ["00","85","AE"];
+    var red = ["B9","28","09"];
+    var green = ["00","91","2D"];
+    var brown = ["9B","7C","57"];
+    var grey = ["80","80","80"];
+    var scale = d3.scaleLinear()
+                    .domain([0, 1])
+                    .range([0, 1])
+                    .clamp(true);
+    var colorscale = d3.scaleLinear()
+                    .domain([0, 255])
+                    .range([0, 255])
+                    .clamp(true);
+    var variance = scale(variance);
+
+    switch(color) {
+        case "blue":
+            var random = Math.floor((Math.random() * variance * 128) - 64 * variance);
+            var newcolor = blue.map(function(d) {return colorscale(parseInt(d, 16) + random)});
+            return "#" + ("0" + newcolor[0].toString(16)).slice(-2) + 
+                         ("0" + newcolor[1].toString(16)).slice(-2) + 
+                         ("0" + newcolor[2].toString(16)).slice(-2);
+        case "red":
+            var random = Math.floor((Math.random() * variance * 128) - 64 * variance);
+            var newcolor = red.map(function(d) {return colorscale(parseInt(d, 16) + random)});
+            return "#" + ("0" + newcolor[0].toString(16)).slice(-2) + 
+                         ("0" + newcolor[1].toString(16)).slice(-2) + 
+                         ("0" + newcolor[2].toString(16)).slice(-2);
+        case "green":
+            var random = Math.floor((Math.random() * variance * 128) - 64 * variance);
+            var newcolor = green.map(function(d) {return colorscale(parseInt(d, 16) + random)});
+            return "#" + ("0" + newcolor[0].toString(16)).slice(-2) + 
+                         ("0" + newcolor[1].toString(16)).slice(-2) + 
+                         ("0" + newcolor[2].toString(16)).slice(-2);
+        case "grey":
+            var random = Math.floor((Math.random() * variance * 128) - 64 * variance);
+            var newcolor = grey.map(function(d) {return colorscale(parseInt(d, 16) + random)});
+            return "#" + ("0" + newcolor[0].toString(16)).slice(-2) + 
+                         ("0" + newcolor[1].toString(16)).slice(-2) + 
+                         ("0" + newcolor[2].toString(16)).slice(-2);
+        case "brown":
+            var random = Math.floor((Math.random() * variance * 128) - 64 * variance);
+            var newcolor = brown.map(function(d) {return colorscale(parseInt(d, 16) + random)});
+            return "#" + ("0" + newcolor[0].toString(16)).slice(-2) + 
+                         ("0" + newcolor[1].toString(16)).slice(-2) + 
+                         ("0" + newcolor[2].toString(16)).slice(-2);
+        default:
+            return "#" + grey[0] + grey[1] + grey[2];
+    }
+}
+
+
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
+
+function queryStringObject() {
+    var QSO = {};
+    var query = window.location.search;
+    if (query.substring(0,1) === "?") {
+        query = query.slice(1);
+    }
+    var queryarray = decodeURI(query).split(/[&|=]/);
+    var keys = queryarray.filter(function(d, i) {return (i + 1) % 2});
+    var values = queryarray.filter(function(d, i) {return i % 2});
+
+    if (keys.length > 0) {
+        if (keys.length > values.length) {
+            for (var i = values.length - 1; i >= 0; i--) {
+                if (Boolean(keys[i])) {
+                    QSO[keys[i]] = values[i];
+                }
+            }    
+        } else {
+            for (var i = keys.length - 1; i >= 0; i--) {
+                if (Boolean(keys[i])) {
+                    QSO[keys[i]] = values[i];
+                }
+            }
+        }
+        
+    }
+    return QSO;
+}
 
 
 
 
-
-
-
-
-
-//*****************************************************************
+//*******************************************************************************************
 //   OTHER FUNCTIONS
-//*****************************************************************
+//*******************************************************************************************
 
 
 function provinceClick(evt) {
@@ -259,7 +320,7 @@ function loadXML() {
             populateFEDList(electionsXML);
     	}
   	};
-  	xhttp.open("GET", "/data/FED2015.xml", true);
+  	xhttp.open("GET", siteroot + "/data/FED2015.xml", true);
   	xhttp.send();
 } 
 
@@ -268,19 +329,10 @@ function loadXML() {
 
 
 
-function getColorBlue(d) {
-    return d > 100000000000 ? '#244A57' :
-           d > 50000000000  ? '#114F54' :
-           d > 1000000000  ? '#497878' :
-           d > 800000000  ? '#00727F' :
-           d > 300000000   ? '#279788' :
-           d > 100000000   ? '#5BB4B0' :
-           d > 40000000   ? '#7CDAD3' :
-                      '#AFE0E0';
-}
 
 
 
+//FOR LEAFLET MAP STYLE OPTION
 function style(feature) {
     return {
         fillColor: getColorBlue(feature.properties.area),
@@ -297,21 +349,46 @@ function coord2FED (point) {
     var provincelist = ["AB", "BC", "SK", "MB", "ON", "QC", "NB", "NS", "NL", "PE", "YT", "NT", "NU"]
     var fedID;
 
+    provinceloop:
     for (var i = provincelist.length - 1; i >= 0; i--) {
-        var mapdataURI = "../data/shapefiles/" + 
+        var mapdataURI = siteroot + "/data/shapefiles/" + 
                          provincelist[i] + "/" + 
                          provincelist[i] + "-multiPart-simplified.json";
-        d3.json(mapdataURI, function (er, mapdata) {
+        d3.json(mapdataURI, function (e, mapdata) {
             var features = mapdata.features;
-            features.forEach(function (f) {
-                if(d3.geoContains(f, point)) {
-                    fedID = f.properties.FEDUID;
-                    console.log(fedID);
+
+            featureloop:
+            for (var i = features.length - 1; i >= 0; i--) {
+                if (d3.geoContains(features[i], point)) {
+                    fedID = features[i].properties.FEDUID;
+                    var queryString = "?fedid=" + String(fedID);
+                    var url = encodeURI(siteroot + "/FEDs" + queryString);
+                    window.location = url;
+                    break featureloop;
                 }
-            });        
+            }
+
+
+
+            // features.forEach(function (f) {
+            //     if(d3.geoContains(f, point)) {
+            //         fedID = f.properties.FEDUID;
+            //         break;
+            //     }
+            // });
+            // if (Boolean(fedID)) {
+            //     var queryString = "?fedid=" + String(fedID);
+            //     var url = encodeURI(siteroot + "/FEDs" + queryString);
+            //     window.location = url;
+            //     return;
+            // } else {
+            //     window.location = siteroot + "/#province-map";
+            //     callBadAddressPopup();
+            //     return;
+            // }
+            return;   
         });
     }
-    return fedID;
 }
 
 
@@ -323,7 +400,7 @@ function createFEDListItem(fedid, fedelement) {
     var fedtitlebarflex = fedsnapshotflex.append("div").classed("FED-titlebar-flex", true);
     var fedbodyflex = fedsnapshotflex.append("div").classed("FED-body-flex", true);
     var fedprovinceabbrev = fedtitlebarflex.append("p").classed("FED-province-abbrev", true).html(fedelement.getElementsByTagName("Province")[0].getAttribute("abbreviation"));
-    var fedname = fedtitlebarflex.append("p").classed("FED-name", true).html(fedelement.getElementsByTagName("Name")[0].childNodes[0].nodeValue);
+    var fedname = fedtitlebarflex.append("p").classed("FED-name", true).attr("id", fedelement.getAttribute("id")).html(fedelement.getElementsByTagName("Name")[0].childNodes[0].nodeValue);
     var fedphotoflex = fedbodyflex.append("div").classed("FED-photo-flex", true);
     fedphotoflex.append("img").attr("src", fedelement.getElementsByTagName("OfficialMPPhoto")[0].childNodes[0].nodeValue);
     var feddetailsflex = fedbodyflex.append("div").classed("FED-details-flex", true);
@@ -341,14 +418,21 @@ function createFEDListItem(fedid, fedelement) {
 
     var raceresultdata = getRaceResultData(fedelement);
     var candidateinfo = fedresultsflex.append("div").classed("candidate-info", true);
-    var raceresultsvg = fedresultsflex.append("svg");
+    var raceresultsvg = fedresultsflex.append("svg").attr("id", "province-svg");
 
-
-
+    var candidatename = createFEDDetailsRow("cand-name", "Candidate", winningcandidate.getElementsByTagName("CandidateName")[0].childNodes[0].nodeValue);
+    var candidatevotes = createFEDDetailsRow("cand-votes", "Percentage Vote", winningcandidate.getElementsByTagName("PercentOfVote")[0].childNodes[0].nodeValue);
+    var candidateoccupation = createFEDDetailsRow("cand-occupation", "Occupation", winningcandidate.getElementsByTagName("Occupation")[0].childNodes[0].nodeValue);
+    var candidateaffiliation = createFEDDetailsRow("cand-affiliation", "Affiliation", winningcandidate.getElementsByTagName("CandidateAffiliation")[0].childNodes[0].nodeValue);
+    candidateinfo.append(function() {return candidatename});
+    candidateinfo.append(function() {return candidateaffiliation});
+    candidateinfo.append(function() {return candidateoccupation});
+    candidateinfo.append(function() {return candidatevotes});
+    
 
 
     var fedturnout = fedstatsflex.append("div").classed("FED-turnout", true);
-    var fedturnoutpercentage = fedturnout.append("p").classed("FED-stats-percentage", true).html(fedelement.getElementsByTagName("Turnout")[0].childNodes[0].nodeValue);
+    var fedturnoutpercentage = fedturnout.append("p").classed("FED-stats-percentage", true).html(fedelement.getElementsByTagName("Turnout")[0].childNodes[0].nodeValue + " %");
     var fedturnoutlabel = fedturnout.append("p").classed("FED-stats-label", true).html("TURNOUT");
 
 
@@ -356,14 +440,9 @@ function createFEDListItem(fedid, fedelement) {
     var fedcompetitionpercentage = fedcompetition.append("p").classed("FED-stats-percentage", true).html(fedelement.getElementsByTagName("Competitiveness")[0].childNodes[0].nodeValue.slice(0,4));
     var fedcompetitionlabel = fedcompetition.append("p").classed("FED-stats-label", true).html("COMPETITIVENESS");
 
-
-
-
-
     createBarChart(raceresultsvg, raceresultdata);
 }
 
-//MAKE THIS AN OBJECT JSON WITH D.NAME, D.VOTE, D.OCCUPATION, D.AFFILIATION
 function getRaceResultData(fedelement) {
     var candidates = fedelement.getElementsByTagName("Candidate");
     var data = [];
@@ -378,7 +457,7 @@ function getRaceResultData(fedelement) {
 
 
 function populateFEDList(electionsXML) {
-    var province = provinceFromQueryString();
+    var province = queryStringObject().province;
     var fedlist = electionsXML.getElementsByTagName("FED");
     for (var i = fedlist.length - 1; i >= 0; i--) {
         var abbrev = fedlist[i].getElementsByTagName("Province")[0].getAttribute("abbreviation");
@@ -410,7 +489,7 @@ function createFEDDetailsRow(id, label, data) {
     datarowlabel.id = id + "-label";
     datarowlabel.innerHTML = String(label);
     datarowdata.className = "FED-data-row-data";
-    datarowdata.id = id + "data";
+    datarowdata.id = id + "-data";
     datarowdata.innerHTML = String(data);
 
     datarow.append(datarowlabel);
@@ -427,18 +506,37 @@ function createBarChart(svg, data) {
     var w = 200;
     var h = 120;
     var barpadding = 2;
+    var boxsize = 14;
+    var boxmargin = 2;
+    var barheight = h - boxsize - boxmargin;
 
     svg.attr("width", w).attr("height", h)
-    svg.selectAll("rect")
-       .data(data)
-       .enter()
-       .append("rect")
-       .classed("histo-bar", true)
-       .attr("x", function(d, i) {return i * w / data.length})
-       .attr("y", function(d) {return h - d.vote})
-       .attr("width", w / data.length - barpadding)
-       .attr("height", function(d, i) {return d.vote/100 * h})
+    var groups = svg.selectAll("g").data(data).enter().append("g").classed("histo-bar", true);
+    groups.append("rect")
+          .classed("histo-bar-bottom", true)
+          .attr("x", function(d, i) {return i * w / data.length})
+          .attr("y", h - boxsize)
+          .attr("width", w / data.length - barpadding)
+          .attr("height", boxsize);
+    groups.append("rect")
+          .classed("histo-bar-top", true)
+          .attr("x", function(d, i) {return i * w / data.length})
+          .attr("y", function(d) {var max = getMaxOfArray(data.map(function(d) {return d.vote}));return barheight - barheight * d.vote / max})
+          .attr("width", w / data.length - barpadding)
+          .attr("height", function(d, i) {var max = getMaxOfArray(data.map(function(d) {return d.vote}));return d.vote / max * barheight});
+    svg.selectAll("g").each(function(d) {d3.select(this).attr("fill", getRandomColor("brown", 0.3)).on("click", function(d) {updateCandidateInfo(d)})});
 }
 
 
+
+
+
+
+function updateCandidateInfo(d) {
+    var candinfo = d3.select(d3.event.target.parentNode.parentNode.parentNode).select(".candidate-info");
+    candinfo.select("#cand-name-data").html(d.name);
+    candinfo.select("#cand-votes-data").html(d.vote);
+    candinfo.select("#cand-affiliation-data").html(d.affiliation);
+    candinfo.select("#cand-occupation-data").html(d.occupation);
+}
 
